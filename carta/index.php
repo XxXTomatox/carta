7bdacaa24dc63b924d0169d0e825a46659063541
XxXTomatox <107597947+XxXTomatox@users.noreply.github.com>
2023-01-25 13:22:44 -0600

    Add files via upload

---
+++ carta/index.php
@@ -0,0 +1,37 @@
+<?php include "./header.php"; ?>
+<br>
+<div class="container p-3 mb-2 bg-info text-dark">
+    <div class="row">
+        <div class="col">
+            <p class="fs-2 text-start" >carta para mi compañero Erwin</p>
+            
+        </div>
+        <div class="col">
+            <p class="fs-2 text-end" >
+                25/01/23
+            </p>
+            
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col ">
+            <p class="fw-normal fs-5 text-md-start" >
+            compañero y mejor amigo Erwin llevo hablando con tigo como 4 añor y en este tiempo puedo derir que eres la amistad mas duradera que e tenido
+            y gracias a eso puedo confirmar que solos los mejores amigos y que siempre puedo confiar en ti gracias por aserme reir tambien 
+            se qu aunque ya no hablamos tanto como antes siempre seras mi mejor amigo 
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col text-center">
+        <img src="324257901_582892160538000_4078290868114642727_n.jpg" class="img-fluid " alt="" width="500px">
+        </div>
+    </div>
+</div>
+
+
+
+<?php 
+    include "./food.php"
+?>
